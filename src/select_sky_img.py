85ad90e9782f36938af7631e80260f6432d51f52
Amanda Li <glitch8077@gmail.com>
2025-02-15 20:13:23 -0800

    WORKING VERSION - Can successfully increase the brightness & saturation of an img in the dataset

--- src/select_sky_img.py
+++ src/select_sky_img.py
@@ -29,8 +29,8 @@ dataset = load_dataset("MultimodalUniverse/legacysurvey", split="train", streami
 
 # Define constant values
 TRUE_BLACK = (0,0,0)
-BRIGHTNESS_INCREASE = 50
-SATURATION_SCALE = 1.5
+BRIGHTNESS_INCREASE = 75
+SATURATION_SCALE = 10
 
 # Process the imgs & convert them to cartoonish versions
 processed_imgs = []
@@ -39,58 +39,6 @@ processed_imgs = []
 for i, data in enumerate(dataset):
     if i >= 1:
         break
-    #print("GETS TO HEREE!!! ---------------------------------------------------------------------")
-
-    
-    # decode the img using Image feature
-    #img_dict = data['image']
-    #pil_img = Image.open(img_dict['path'])
-    
-
-
-
-
-
-    
-    # pil_img = data['image']
-
-    # # convert the PIL img to NumPY array w/ dtype uint8
-    # img = np.array(pil_img, dtype=np.uint8)
-
-    # #### use OpenCV to load in the img
-    # ####img = cv2.cvtColor(np.array(data['image']), cv2.COLOR_RGB2BGR)
-
-    # # convert to rgb colors
-    # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    # # convert img to HSV colors
-    # hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    # # identify bright points & create mask for them
-    # _, brightness_mask = cv2.threshold(hsv_img[:,:,2], 50, 255, cv2.THRESH_BINARY)
-
-    # # apply TRUE_BLACK to unmasked regions
-    # img[brightness_mask == 0] = TRUE_BLACK
-
-    # # inc. saturation of the masked (bright) regions
-    # hsv_img[:,:,1] = np.where(
-    #     brightness_mask == 255,
-    #     np.clip(hsv_img[:,:,1] * SATURATION_SCALE, 0, 255).astype(hsv_img.dtype),
-    #     hsv_img[:,:,1]
-    # )
-
-    # # convert back to rgb
-    # img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
-
-    # # store the processed img
-    # processed_imgs.append(img)
-
-    # # DEBUG STATEMENT ----------------------------------------------------------
-    # cv2.imwrite(f"processed_img_{i}.png", img)
-
-
-
-
 
     # TEST CODE -----------------------------------------
     img_path = "GalaxyTest IMG.jpg"
@@ -107,14 +55,30 @@ for i, data in enumerate(dataset):
     # apply TRUE_BLACK to darker (unmasked) pixels
     img[~bright_points_mask] = TRUE_BLACK
 
+    # # increase brightness of remaining points
+    # img[bright_points_mask] = cv2.add(img[bright_points_mask], BRIGHTNESS_INCREASE)
+    # img = np.clip(img, 0, 255)  # ensured pixel values remain valid
+
+
+
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h,s,v = cv2.split(hsv_img)
+
+    s = s.astype(np.float32)
+    s *= SATURATION_SCALE
+
+    s = np.clip(s, 0, 255)
+    s = s.astype(np.uint8)
+
+    hsv_img = cv2.merge([h,s,v])
+    enhanced_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+    
     # increase brightness of remaining points
-    img[bright_points_mask] = cv2.add(img[bright_points_mask], BRIGHTNESS_INCREASE)
-    img = np.clip(img, 0, 255)  # ensured pixel values remain valid
+    enhanced_img[bright_points_mask] = cv2.add(enhanced_img[bright_points_mask], BRIGHTNESS_INCREASE)
+    enhanced_img = np.clip(enhanced_img, 0, 255)  # ensured pixel values remain valid
 
     # store the processed img
-    processed_imgs.append(img)
+    processed_imgs.append(enhanced_img)
 
     # DEBUG STATEMENT ----------------------------------------------------------
     cv2.imwrite(f"processed_img_{i}.png", img)
-
-
